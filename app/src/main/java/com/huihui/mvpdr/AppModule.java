f3e69db701b10f3fbbfc0d351aaaafc88343ff39
gavin <molu_clown@163.com>
2016-11-17 18:16:06 +0800

    初始化项目

---
+++ app/src/main/java/com/huihui/mvpdr/AppModule.java
@@ -0,0 +1,29 @@
+package com.huihui.mvpdr;
+
+import android.app.Application;
+
+import javax.inject.Singleton;
+
+import dagger.Provides;
+
+/**
+ * Author: gavin
+ * Date: 2016/11/17
+ * Time: 18:00
+ * Email:2415580905@qq.com
+ */
+public class AppModule {
+
+    private Application mApplication;
+
+
+    public AppModule(Application mApplication) {
+        this.mApplication = mApplication;
+    }
+
+    @Singleton
+    @Provides
+    Application providesApplication() {
+        return mApplication;
+    }
+}
